44193cbebdcfab80693941cfc5c7867c683b927e
Daisuke Fujita <dtanshi45@gmail.com>
2020-04-06 22:25:25 +0900

    Add logger

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -23,6 +24,10 @@ func HandleRequest(ctx context.Context) error {
 	bucket := os.Getenv("BUCKET")
 	keyPrefix := os.Getenv("KEY_PREFIX")
 
+	log.Printf("url: %q", url)
+	log.Printf("bucket: %q", bucket)
+	log.Printf("key prefix: %q", keyPrefix)
+
 	return do(ctx, url, bucket, keyPrefix)
 }
 
@@ -34,6 +39,8 @@ func do(ctx context.Context, url, bucket, keyPrefix string) error {
 	httpClient := http.Client{
 		Timeout: 5 * time.Second,
 	}
+
+	log.Printf("downloading %s", url)
 
 	body, ext, err := download(ctx, httpClient, url)
 	if err != nil {
@@ -48,6 +55,8 @@ func do(ctx context.Context, url, bucket, keyPrefix string) error {
 	if ext != "" {
 		key += "." + ext
 	}
+
+	log.Printf("uploading to bucket: %s key: %s", bucket, key)
 
 	if err := s3Client.UploadToS3(ctx, bucket, key, bytes.NewReader(body)); err != nil {
 		return fmt.Errorf("cannot upload downloaded file to S3 (bucket: %q, key: %q): %w", bucket, key, err)
