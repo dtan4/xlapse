35d4e07f4e41f5305821d09b685db25c93e57734
Daisuke Fujita <dtanshi45@gmail.com>
2020-04-08 01:17:49 +0900

    Download S3 object in distributor

--- distributor/main.go
+++ distributor/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
@@ -13,12 +18,26 @@ func main() {
 }
 
 func realMain(args []string) error {
-	if len(args) < 2 {
-		return fmt.Errorf("object path is missing")
+	if len(args) < 3 {
+		return fmt.Errorf("bucket and/or key is missing")
 	}
-	path := args[1]
+	bucket, key := args[1], args[2]
 
-	fmt.Println(path)
+	log.Printf("bucket: %q", bucket)
+	log.Printf("key: %q", key)
+
+	ctx := context.Background()
+
+	sess := session.New()
+	api := s3.New(sess)
+	s3Client := newS3Client(api)
+
+	body, err := s3Client.GetObject(ctx, bucket, key)
+	if err != nil {
+		return fmt.Errorf("cannot download file from S3 (bucket: %q, key: %q): %w", bucket, key, err)
+	}
+
+	fmt.Println(string(body))
 
 	return nil
 }
