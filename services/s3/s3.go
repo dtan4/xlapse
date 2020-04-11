c39266cbb218c601b1c0b5674e4a8e8d917dd585
Daisuke Fujita <dtanshi45@gmail.com>
2020-04-12 01:04:48 +0900

    Use composeFolder from ComposeKey

--- services/s3/s3.go
+++ services/s3/s3.go
@@ -85,12 +85,16 @@ func (c *Client) Upload(ctx context.Context, bucket, key string, reader io.ReadS
 
 // {prefix}/2006/01/02/
 func ComposeFolder(prefix string, year, month, day int) string {
+	return composeFolder(prefix, year, month, day)
+}
+
+func composeFolder(prefix string, year, month, day int) string {
 	return filepath.Join(prefix, fmt.Sprintf("%04d/%02d/%02d", year, month, day)) + "/"
 }
 
 // {prefix}/2006/01/02/2006-01-02-15-04-00.png
 func ComposeKey(prefix string, now time.Time, ext string) string {
-	key := filepath.Join(prefix, fmt.Sprintf("%04d/%02d/%02d", now.Year(), now.Month(), now.Day()), now.Format(timeFormat))
+	key := filepath.Join(composeFolder(prefix, now.Year(), int(now.Month()), now.Day()), now.Format(timeFormat))
 	if ext != "" {
 		key += "." + ext
 	}
