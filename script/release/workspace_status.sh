34a652cc978b11075123a9e4a84ba9170a9a7a16
Daisuke Fujita <dtanshi45@gmail.com>
2021-09-04 03:50:38 +0900

    Support rules_go stamping to embed metadata to binary

---
+++ script/release/workspace_status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Stamping with the workspace status script (replacement of -ldflags)
+# https://github.com/bazelbuild/rules_go/blob/70b8365a90e226b98f66bc35d3abb3e335883d81/go/core.rst#stamping-with-the-workspace-status-script
+
+main() {
+  set -eu
+  set -o pipefail
+
+  # https://docs.github.com/en/actions/reference/environment-variables
+  echo RELEASE_VERSION "$(printf "%s" "${GITHUB_REF}" | sed -E "s/^v//")"
+  echo RELEASE_COMMIT "${GITHUB_SHA}"
+  echo RELEASE_DATE "$(TZ=UTC date "+%Y-%m-%dT%H:%M:%SZ")"
+}
+
+main
