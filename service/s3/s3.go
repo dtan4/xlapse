d8d95cb39ecfa157e32b6fd9f85c84b411c19bba
Daisuke Fujita <dtanshi45@gmail.com>
2021-09-04 15:58:40 +0900

    Remove S3 client with SDK V1

--- service/s3/s3.go
+++ service/s3/s3.go
@@ -10,9 +10,6 @@ import (
 
 	awsv2 "github.com/aws/aws-sdk-go-v2/aws"
 	s3v2 "github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 )
 
 const (
@@ -95,72 +92,6 @@ func (c *ClientV2) Upload(ctx context.Context, bucket, key string, reader io.Rea
 	_, err := c.api.PutObject(ctx, &s3v2.PutObjectInput{
 		Bucket: awsv2.String(bucket),
 		Key:    awsv2.String(key),
-		Body:   reader,
-	})
-	if err != nil {
-		return fmt.Errorf("cannot upload file to S3: %w", err)
-	}
-
-	return nil
-}
-
-// Client represents the wrapper of S3 API Client
-type Client struct {
-	api s3iface.S3API
-}
-
-// New creates new Client
-func New(api s3iface.S3API) *Client {
-	return &Client{
-		api: api,
-	}
-}
-
-// ListObjectKeys retrieves the list of keys in the given S3 bucket and folder
-func (c *Client) ListObjectKeys(ctx context.Context, bucket, folder string) ([]string, error) {
-	keys := []string{}
-
-	err := c.api.ListObjectsV2PagesWithContext(ctx, &s3.ListObjectsV2Input{
-		Bucket: aws.String(bucket),
-		Prefix: aws.String(folder),
-	}, func(page *s3.ListObjectsV2Output, lastPage bool) bool {
-		for _, c := range page.Contents {
-			keys = append(keys, aws.StringValue(c.Key))
-		}
-
-		return true
-	})
-	if err != nil {
-		return []string{}, fmt.Errorf("cannot retrieve object list from S3 (bucket: %q, folder: %q): %w", bucket, folder, err)
-	}
-
-	return keys, nil
-}
-
-// UploadToS3 uploads local file to the specified S3 location
-func (c *Client) GetObject(ctx context.Context, bucket, key string) ([]byte, error) {
-	out, err := c.api.GetObjectWithContext(ctx, &s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-	})
-	if err != nil {
-		return []byte{}, fmt.Errorf("cannot download S3 object from bucket: %q, key: %q: %w", bucket, key, err)
-	}
-	defer out.Body.Close()
-
-	body, err := ioutil.ReadAll(out.Body)
-	if err != nil {
-		return []byte{}, fmt.Errorf("cannot read S3 object from bucket: %q, key: %q: %w", bucket, key, err)
-	}
-
-	return body, nil
-}
-
-// Upload uploads the given stream to the given S3 location
-func (c *Client) Upload(ctx context.Context, bucket, key string, reader io.ReadSeeker) error {
-	_, err := c.api.PutObjectWithContext(ctx, &s3.PutObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
 		Body:   reader,
 	})
 	if err != nil {
